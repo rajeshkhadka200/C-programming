dd5887d206192ae25651999280d801e35b0994aa
Rajesh khadka <69001910+rajeshkhadka200@users.noreply.github.com>
2024-12-31 18:27:37 +0545

    added the new program

--- user_desire_repeat_palendrome.c
+++ user_desire_repeat_palendrome.c
@@ -1,40 +1,31 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
-// print fact 
-int calcfact () {
-    
+// calc and print fact
+void calcfact() {
     int num;
-    
-    printf("Enter the number : ");
+
+    printf("Enter the number: ");
     scanf("%d", &num);
-    
+
     int fact = 1;
-    for(int i=1; i<=num; i++){
-        fact = fact *i;    
+    for (int i = 1; i <= num; i++) {
+        fact *= i;
     }
-    printf("The fact of the num is : %d \n", fact);
+    printf("The factorial of the number is: %d\n", fact);
 }
 
-int ask (){
-    
+// ask 
+void ask() {
     int again;
-    printf("Press 1 to continue, press 0 to exit.\n");
-    scanf("%d", &again);
-    
-    if(again == 1){
-     calcfact();   
-    }else{
-        return 0;
-    }
+    do {
+        calcfact();
+        printf("Press 1 to continue, press 0 to exit.\n");
+        scanf("%d", &again);
+    } while (again == 1);
 }
 
-// main function
-
+// Main function
 int main() {
-    
-    calcfact();
     ask();
-    
     return 0;
 }
